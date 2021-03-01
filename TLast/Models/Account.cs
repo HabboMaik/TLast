f4619fd34fb9e343671eda243cbe5eea4f48109f
ric <28328349+Soletrar@users.noreply.github.com>
2021-03-01 15:10:46 -0300

    Criando Modelo Account

---
+++ TLast/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TLast.Models
+{
+    internal class Account
+    {
+        public string Email;
+        public string Password;
+
+        public Account(string account)
+        {
+            var split = account.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            if (split.Length != 2) return;
+
+            Email    = split[0];
+            Password = split[1];
+        }
+
+        public bool IsValid => Email != null && Password != null;
+    }
+}
