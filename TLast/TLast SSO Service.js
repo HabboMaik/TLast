33dcee560099625a7975eec5f77924d004fdf5d3
ric <28328349+Soletrar@users.noreply.github.com>
2021-03-18 15:23:08 -0300

    Adicionado script Javascript para Tampermonkey.

---
+++ TLast/TLast SSO Service.js
@@ -0,0 +1,21 @@
+﻿// ==UserScript==
+// @name         TLast SSO Service
+// @namespace    ric
+// @version      0.1
+// @description  Client by ric
+// @author       ric
+// @match        https://www.habbo.com.br/api/public/captcha*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    let socket = new WebSocket("ws://localhost:27/ric");
+    socket.onopen = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has("token") && urlParams.get("token") !== "") {
+            socket.send(urlParams.get("token"));
+        }
+    }
+})();
